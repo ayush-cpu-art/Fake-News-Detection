9b076f7d4cd886cce33f1d530e0606d27c942993
Ayush <ayushdev0408@gmail.com>
2026-09-24 23:25:50 +0530

    Make API URL configurable

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,11 @@
+import os
 import requests
 import streamlit as st
 
-
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000/predict"
+)
 
 
 st.set_page_config(
